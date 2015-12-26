ae76b60ca5ec03d04b3ea1356f8b21bbccc92ec6
adacola <adacola255@gmail.com>
2015-12-26 16:37:12 +0900

    initial commit

---
+++ Rokuyou/Rokuyou.fs
@@ -0,0 +1,37 @@
+﻿namespace Adacola
+
+type Rokuyou =
+    | Taian = 0
+    | Shakku = 1
+    | Senkachi = 2
+    | Tomobiki = 3
+    | Senmake = 4
+    | Butsumetsu = 5
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Rokuyou =
+    open System
+    open System.Globalization
+
+    [<CompiledName("ToString")>]
+    let toString = function
+        | Rokuyou.Taian -> "大安"
+        | Rokuyou.Shakku -> "赤口"
+        | Rokuyou.Senkachi -> "先勝"
+        | Rokuyou.Tomobiki -> "友引"
+        | Rokuyou.Senmake -> "先負"
+        | Rokuyou.Butsumetsu -> "仏滅"
+        | _ -> invalidArg "" "Rokuyou型の値を指定してください"
+
+    let private ja = JapaneseLunisolarCalendar()
+
+    [<CompiledName("OfDate")>]
+    let ofDate (date : DateTime) =
+        // 参考 : http://blogs.jp.infragistics.com/blogs/dikehara/archive/2010/08/29/wpf-xammonthcalendar-tips.aspx
+        let oldMonth =
+            let newYearDate = ja.AddDays(date, 1 - ja.GetDayOfYear date)
+            let leapMonth = ja.GetLeapMonth(ja.GetYear newYearDate, ja.GetEra newYearDate)
+            let month = ja.GetMonth date
+            if  0 < leapMonth && leapMonth <= month then month - 1 else month
+        let oldDay = ja.GetDayOfMonth date
+        (oldMonth + oldDay) % 6 |> enum<Rokuyou>
---
+++ RokuyouTest/RokuyouTest.fs
@@ -0,0 +1,35 @@
+﻿namespace Adacola.Rokuyou.Test
+
+open FsCheck
+open NUnit.Framework
+
+[<TestFixture>]
+module Rokuyou =
+    open System
+    open System.IO
+    open Adacola
+
+    [<Test>]
+    let ``ofDateで取得した六曜とその文字列が妥当であること`` () =
+        let expectedMap =
+            let lines = File.ReadAllLines(@"RokuyouExpected.tsv", Text.Encoding.UTF8)
+            lines |> Seq.map (fun line ->
+                let splittedLine = line.Split [|'\t'|]
+                let date = DateTime.ParseExact(splittedLine.[0], "yyyy/MM/dd", null)
+                let rokuyou = splittedLine.[2] |> int |> enum<Rokuyou>
+                date, (rokuyou, splittedLine.[1]))
+            |> dict
+
+        let dateTimeGen = gen {
+            let year = 2016
+            let! month = Gen.choose(1, 12)
+            let maxDay = DateTime.DaysInMonth(year, month)
+            let! day = Gen.choose(1, maxDay)
+            return DateTime(year, month, day)
+        }
+
+        Prop.forAll (Arb.fromGen dateTimeGen) (fun dt ->
+            let rokuyou = Rokuyou.ofDate dt
+            let rokuyouStr = Rokuyou.toString rokuyou
+            (rokuyou, rokuyouStr) = expectedMap.[dt])
+        |> Check.QuickThrowOnFailure
